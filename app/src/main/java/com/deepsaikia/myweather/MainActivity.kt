4b6f59d781cd8ad1fda60e00686e3334fbb93fbf
Deep Saikia <96719584+saikiaDeep@users.noreply.github.com>
2023-03-17 12:21:13 +0530

    Update 1

--- app/src/main/java/com/deepsaikia/myweather/MainActivity.kt
+++ app/src/main/java/com/deepsaikia/myweather/MainActivity.kt
@@ -37,147 +37,140 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     //used to get the user's current location
-    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    // private lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     private var mProgressDialog: Dialog? = null
-    private var mLatitude: Double = 0.0
-    private var mLongitude: Double = 0.0
+    private var mLatitude: Double = 19.0760
+    private var mLongitude: Double = 72.8777
     private lateinit var mSharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        getSupportActionBar()?.hide()
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        supportActionBar?.hide()
+        // getLocationWeatherDetails()
 
-        val c= Calendar.getInstance()
-        val hour=c.get(Calendar.HOUR_OF_DAY);
-        if(hour>=18 || hour<=5)
-        {
-            linearLayout.background=ContextCompat.getDrawable(applicationContext,R.drawable.night)
-        }
-        else
-        {
-            linearLayout.background=ContextCompat.getDrawable(applicationContext,R.drawable.day)
-        }
+        // mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+//        val c= Calendar.getInstance()
+//        val hour=c.get(Calendar.HOUR_OF_DAY);
+//        if(hour>=18 || hour<=5)
+//        {
+//            linearLayout.background=ContextCompat.getDrawable(applicationContext,R.drawable.night)
+//        }
+//        else
+//        {
+//            linearLayout.background=ContextCompat.getDrawable(applicationContext,R.drawable.day)
+//        }
 
 
-        //restore the previously stored data
+//        //restore the previously stored data
         mSharedPreferences = getSharedPreferences(Constants.PREFERENCE_NAME, Context.MODE_PRIVATE)
-        Log.e("main string stored earlier",
+        Log.e(
+            "main string stored earlier",
             mSharedPreferences.getString(Constants.WEATHER_RESPONSE_DATA, "").toString()
         )
         setupUI()
+//
+//        if (!isLocationEnabled()) {
+//            Toast.makeText(
+//                this, "Your location provider is turned off. Please turn it on.", Toast.LENGTH_SHORT
+//            ).show()
+//            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+//            startActivity(intent)
+//        } else {
+//            Dexter.withActivity(this).withPermissions(
+//                    Manifest.permission.ACCESS_FINE_LOCATION,
+//                    Manifest.permission.ACCESS_COARSE_LOCATION
+//                ).withListener(object : MultiplePermissionsListener {
+//                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+//                        if (report!!.areAllPermissionsGranted()) {
+//                            requestLocationData()
+//                        }
+//
+//                        if (report.isAnyPermissionPermanentlyDenied) {
+//                            Toast.makeText(
+//                                this@MainActivity,
+//                                "You have denied location permission. Please allow it is mandatory.",
+//                                Toast.LENGTH_SHORT
+//                            ).show()
+//                        }
+//                    }
+//
+//                    override fun onPermissionRationaleShouldBeShown(
+//                        permissions: MutableList<PermissionRequest>?, token: PermissionToken?
+//                    ) {
+//                        showRationalDialogForPermissions()
+//                    }
+//                }).onSameThread().check()
+//        }
 
-        if (!isLocationEnabled()) {
-            Toast.makeText(
-                this,
-                "Your location provider is turned off. Please turn it on.",
-                Toast.LENGTH_SHORT
-            ).show()
-            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-            startActivity(intent)
-        } else {
-            Dexter.withActivity(this)
-                .withPermissions(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                )
-                .withListener(object : MultiplePermissionsListener {
-                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                        if (report!!.areAllPermissionsGranted()) {
-                            requestLocationData()
-                        }
-
-                        if (report.isAnyPermissionPermanentlyDenied) {
-                            Toast.makeText(
-                                this@MainActivity,
-                                "You have denied location permission. Please allow it is mandatory.",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-
-                    override fun onPermissionRationaleShouldBeShown(
-                        permissions: MutableList<PermissionRequest>?,
-                        token: PermissionToken?
-                    ) {
-                        showRationalDialogForPermissions()
-                    }
-                }).onSameThread()
-                .check()
-        }
-        floatingActionButton3.setOnClickListener{
-            getLocationWeatherDetails()
-            setupUI()
-        }
     }
 
+    private fun updateLocation() {
 
-
-
-    // A function which is used to verify that the location or GPS is enable or not of the user's device.
-    private fun isLocationEnabled(): Boolean {
-        val locationManager: LocationManager =
-            getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
     }
 
-    //A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
-    private fun showRationalDialogForPermissions() {
-        AlertDialog.Builder(this)
-            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
-            .setPositiveButton(
-                "GO TO SETTINGS"
-            ) { _, _ ->
-                try {
-                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri = Uri.fromParts("package", packageName, null)
-                    intent.data = uri
-                    startActivity(intent)
-                } catch (e: ActivityNotFoundException) {
-                    e.printStackTrace()
-                }
-            }
-            .setNegativeButton("Cancel") { dialog,
-                                           _ ->
-                dialog.dismiss()
-            }.show()
-    }
+//
+//    // A function which is used to verify that the location or GPS is enable or not of the user's device.
+//    private fun isLocationEnabled(): Boolean {
+//        val locationManager: LocationManager =
+//            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+//            LocationManager.NETWORK_PROVIDER
+//        )
+//    }
+//
+//    //A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+//    private fun showRationalDialogForPermissions() {
+//        AlertDialog.Builder(this)
+//            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+//            .setPositiveButton(
+//                "GO TO SETTINGS"
+//            ) { _, _ ->
+//                try {
+//                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+//                    val uri = Uri.fromParts("package", packageName, null)
+//                    intent.data = uri
+//                    startActivity(intent)
+//                } catch (e: ActivityNotFoundException) {
+//                    e.printStackTrace()
+//                }
+//            }.setNegativeButton("Cancel") { dialog, _ ->
+//                dialog.dismiss()
+//            }.show()
+//    }
 
-    //to request the current location.
-    @Suppress("DEPRECATION")
-    @SuppressLint("MissingPermission")
-    private fun requestLocationData() {
-
-        val mLocationRequest = LocationRequest()
-        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        mFusedLocationClient.requestLocationUpdates(
-            mLocationRequest, mLocationCallback,
-            Looper.myLooper()
-        )
-    }
-
-    //get the current location details.
-    private val mLocationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-
-            val mLastLocation: Location? = locationResult.lastLocation
-            if (mLastLocation != null) {
-                mLatitude = mLastLocation.latitude
-            }
-            Log.e("Current Latitude", "$mLatitude")
-            if (mLastLocation != null) {
-                mLongitude = mLastLocation.longitude
-            }
-            Log.e("Current Longitude", "$mLongitude")
-
-            getLocationWeatherDetails()
-        }
-    }
+//    //to request the current location.
+//    @Suppress("DEPRECATION")
+//    @SuppressLint("MissingPermission")
+//    private fun requestLocationData() {
+//
+//        val mLocationRequest = LocationRequest()
+//        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+//        mFusedLocationClient.requestLocationUpdates(
+//            mLocationRequest, mLocationCallback, Looper.myLooper()
+//        )
+//    }
+//
+//    //get the current location details.
+//    private val mLocationCallback = object : LocationCallback() {
+//        override fun onLocationResult(locationResult: LocationResult) {
+//
+//            val mLastLocation: Location? = locationResult.lastLocation
+//            if (mLastLocation != null) {
+//                mLatitude = mLastLocation.latitude
+//            }
+//            Log.e("Current Latitude", "$mLatitude")
+//            if (mLastLocation != null) {
+//                mLongitude = mLastLocation.longitude
+//            }
+//            Log.e("Current Longitude", "$mLongitude")
+//
+//            getLocationWeatherDetails()
+//        }
+//    }
 
     //details of the current location based on the latitude longitude
 
@@ -186,32 +179,32 @@ class MainActivity : AppCompatActivity() {
         if (Constants.isNetworkAvailable(this@MainActivity)) {
             val retrofit: Retrofit = Retrofit.Builder()
                 // API base URL.
-                .baseUrl(Constants.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(Constants.BASE_URL).addConverterFactory(GsonConverterFactory.create())
                 .build()
-            val service: WeatherService =
-                retrofit.create(WeatherService::class.java)
+            val service: WeatherService = retrofit.create(WeatherService::class.java)
             val listCall: Call<WeatherResponse> = service.getWeather(
                 mLatitude, mLongitude, Constants.METRIC_UNIT, Constants.APP_ID
             )
 
-            showCustomProgressDialog()
+            //showCustomProgressDialog()
 
             listCall.enqueue(object : Callback<WeatherResponse> {
                 @SuppressLint("SetTextI18n")
                 override fun onResponse(
-                    response: Response<WeatherResponse>,
-                    retrofit: Retrofit
+                    response: Response<WeatherResponse>, retrofit: Retrofit
                 ) {
                     if (response.isSuccess) {
 
-                        hideProgressDialog()
+                        //hideProgressDialog()
                         val weatherList: WeatherResponse = response.body()
                         val weatherResponseJsonString = Gson().toJson(weatherList)
                         val editor = mSharedPreferences.edit()
                         editor.putString(Constants.WEATHER_RESPONSE_DATA, weatherResponseJsonString)
-                        Log.e("ds",weatherResponseJsonString)
+                        //Log.e("ds",weatherResponseJsonString)
                         editor.apply()
+                        Log.d("response", weatherResponseJsonString)
+
+                        setupUI()
 
                     } else {
                         when (val sc = response.code()) {
@@ -226,26 +219,29 @@ class MainActivity : AppCompatActivity() {
                             }
                         }
                     }
+                    hideProgressDialog()
                 }
 
                 override fun onFailure(t: Throwable) {
-                    hideProgressDialog() // Hides the progress dialog
+                    //hideProgressDialog() // Hides the progress dialog
                     Log.e("Errorrrrr", t.message.toString())
+                    hideProgressDialog()
                 }
             })
         } else {
             Toast.makeText(
-                this@MainActivity,
-                "No internet connection available.",
-                Toast.LENGTH_SHORT
+                this@MainActivity, "No internet connection available.", Toast.LENGTH_SHORT
             ).show()
+            hideProgressDialog()
         }
     }
+
     private fun showCustomProgressDialog() {
         mProgressDialog = Dialog(this)
         mProgressDialog!!.setContentView(R.layout.dialog_custom_progress)
         mProgressDialog!!.show()
     }
+
     private fun hideProgressDialog() {
         if (mProgressDialog != null) {
             mProgressDialog!!.dismiss()
@@ -255,40 +251,30 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun setupUI() {
-           val weatherResponseJsonString =
+        val weatherResponseJsonString =
             mSharedPreferences.getString(Constants.WEATHER_RESPONSE_DATA, "")
 
         if (!weatherResponseJsonString.isNullOrEmpty()) {
 
             val weatherList =
                 Gson().fromJson(weatherResponseJsonString, WeatherResponse::class.java)
-            if(weatherList.name=="Globe")
-            {
-                Toast.makeText(
-                    this,
-                    "Press refresh to start",
-                    Toast.LENGTH_SHORT
-                ).show()
-                return
-            }
-            for (z in weatherList.weather.indices) {
-                tv_main.text = weatherList.weather[z].main
-                tv_main_description.text = weatherList.weather[z].description
-                tv_temp.text =
-                    weatherList.main.pressure.toString() + " P"
+            for (index in weatherList.weather.indices) {
+                tv_main.text = weatherList.weather[index].main
+                tv_main_description.text = weatherList.weather[index].description
+                tv_temp.text = weatherList.main.pressure.toString() + " P"
                 tv_humidity.text = weatherList.main.humidity.toString() + "%"
                 tv_min.text = weatherList.main.temp.toString() + " °C"
-                tv_max.text = "RF "+ weatherList.main.feels_like.toString() + " °C"
+                tv_max.text = "RF " + weatherList.main.feels_like.toString() + " °C"
                 tv_speed.text = weatherList.wind.speed.toString()
                 tv_name.text = weatherList.name
                 tv_country.text = weatherList.sys.country
                 tv_sunrise_time.text = unixTime(weatherList.sys.sunrise.toLong())
                 tv_sunset_time.text = unixTime(weatherList.sys.sunset.toLong())
-                Latitude.text="Latitude : "+weatherList.coord.lat.toString()
-                Longitude.text="Longitude : "+weatherList.coord.lon.toString()
-                time.text="Time of data calculation "+unixTime(weatherList.dt.toLong())
+                Latitude.text = "Latitude : " + weatherList.coord.lat.toString()
+                Longitude.text = "Longitude : " + weatherList.coord.lon.toString()
+                time.text = "Time of data calculation " + unixTime(weatherList.dt.toLong())
                 // Here we update the main icon
-                when (weatherList.weather[z].icon) {
+                when (weatherList.weather[index].icon) {
                     "01d" -> iv_main.setImageResource(R.drawable.sunny)
                     "02d" -> iv_main.setImageResource(R.drawable.cloud)
                     "03d" -> iv_main.setImageResource(R.drawable.cloud)
@@ -305,14 +291,9 @@ class MainActivity : AppCompatActivity() {
                     "13n" -> iv_main.setImageResource(R.drawable.snowflake)
                 }
             }
-        }
-        else
-        {
-            Toast.makeText(
-                this,
-                "Press refresh to start",
-                Toast.LENGTH_SHORT
-            ).show()
+        } else {
+            showCustomProgressDialog()
+            getLocationWeatherDetails()
         }
 
 
@@ -321,8 +302,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun unixTime(timex: Long): String? {
         val date = Date(timex * 1000L)
-        @SuppressLint("SimpleDateFormat") val sdf =
-            SimpleDateFormat("hh:mm a")
+        @SuppressLint("SimpleDateFormat") val sdf = SimpleDateFormat("hh:mm a")
         sdf.timeZone = TimeZone.getDefault()
         return sdf.format(date)
     }
---
+++ app/src/main/java/com/deepsaikia/myweather/utils/LocationProvider.kt
@@ -0,0 +1,21 @@
+package com.deepsaikia.myweather.utils
+
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+
+class LocationProvider {
+
+}
